683b926db27cac6504d9f1eadd430f4c8b33f55c
NandoPL <93694678+NandoPL@users.noreply.github.com>
2021-11-17 16:30:27 -0600

    Add files via upload

---
+++ L4.py
@@ -0,0 +1,106 @@
+# Solución del Laboratorio 4
+# Problema 4
+
+# Los parámetros T, t_final y N son elegidos arbitrariamente
+
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+
+#######################################################
+################# Parte a) ############################
+### C y Z variables aleatorias, Omega constante = w ###
+#######################################################
+# Variables aleatorias C y Z
+vaC = stats.norm(5, np.sqrt(0.2))
+vaZ = stats.uniform(0, np.pi/2)
+
+# Omega no es Constante aleatoria, sino que una constante w
+# w debe estar dentro de 2𝜋(59,1) y 2𝜋(60,1)], por lo que se toma un valor
+# dentro del rango
+w = 2 * np.pi * 59.6
+
+# Creación del vector de tiempo
+T = 100			# número de elementos
+t_final = 10	# tiempo en segundos
+t = np.linspace(0, t_final, T)
+
+# Inicialización del proceso aleatorio X(t) con N realizaciones
+N = 10
+X_t = np.empty((N, len(t)))	# N funciones del tiempo x(t) con T puntos
+
+# Creación de las muestras del proceso x(t) (C y Z independientes)
+for i in range(N):
+	C = vaC.rvs()
+	Z = vaZ.rvs()
+	x_t = C * np.cos(w*t + Z)
+	X_t[i,:] = x_t
+	plt.plot(t, x_t)
+
+# Promedio de las N realizaciones en cada instante (cada punto en t)
+P = [np.mean(X_t[:,i]) for i in range(len(t))]
+plt.plot(t, P, lw=6)
+
+# Graficar el resultado teórico del valor esperado
+E = 10/np.pi * (np.cos(w*t) - np.sin(w*t))
+plt.plot(t, E, '-.', lw=4)
+
+# Mostrar las realizaciones, y su promedio calculado y teórico
+plt.title('Realizaciones del proceso aleatorio $X(t)$')
+plt.xlabel('$t$')
+plt.ylabel('$x_i(t)$')
+plt.show()
+
+
+
+#######################################################
+################# Parte b) ############################
+### C variable aleatoria, Omega y Z constantes ########
+#######################################################
+
+# Variable aleatoria C
+vaC = stats.norm(5, np.sqrt(0.2))
+
+# Omega no es Constante aleatoria, sino que una constante w
+# Z tamben se vuelve una constante entre 0 y  𝜋/2
+w = 2 * np.pi * 59.6
+Zb =  np.pi/4
+
+# Angulo theta = constante Zb
+theta = Zb
+
+
+# Creación de las muestras del proceso x(t) (C independiente)
+for i in range(N):
+	C = vaC.rvs()
+	x_t = C * np.cos(w*t + theta)
+	X_t[i,:] = x_t
+	#plt.plot(t, x_t)
+
+
+# T valores de desplazamiento tau
+desplazamiento = np.arange(T)
+taus = desplazamiento/t_final
+
+# Inicialización de matriz de valores de correlación para las N funciones
+corr = np.empty((N, len(desplazamiento)))
+
+# Nueva figura para la autocorrelación
+plt.figure()
+
+# Cálculo de correlación para cada valor de tau
+for n in range(N):
+	for i, tau in enumerate(desplazamiento):
+		corr[n, i] = np.correlate(X_t[n,:], np.roll(X_t[n,:], tau))/T
+	plt.plot(taus, corr[n,:])
+
+# Valor teórico de correlación
+Rxx = 25.5 * np.cos(w*t + theta) * np.cos(w * (t + taus) + theta)
+
+# Gráficas de correlación para cada realización y la
+plt.plot(taus, Rxx, '-.', lw=4, label='Correlación teórica')
+plt.title('Funciones de autocorrelación de las realizaciones del proceso')
+plt.xlabel(r'$\tau$')
+plt.ylabel(r'$R_{XX}(\tau)$')
+plt.legend()
+plt.show()
